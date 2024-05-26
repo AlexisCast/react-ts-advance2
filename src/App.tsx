9fdf879b61d62a066ada1429e13b48b2e4f2ac26
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-26 16:38:32 -0600

    create Button comopnent that renders different elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import Input from "./components/Input";
+import Button from "./components/Button.tsx";
+import Input from "./components/Input.tsx";
 
 function App() {
 	return (
 		<main>
 			<Input id="name" label="Your name" type="text" />
 			<Input id="age" label="Your age" type="number" />
+			<p>
+				<Button el="button">A Button</Button>
+			</p>
+			<p>
+				<Button el="anchor" href="https://google.com">
+					A Link
+				</Button>
+			</p>
 		</main>
 	);
 }
