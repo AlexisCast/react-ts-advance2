d192b9b689f6748d9adce65f7cee89d7aa2b54af
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-30 19:58:14 -0600

    implement useReducer and actions

--- src/store/timers-contest.tsx
+++ src/store/timers-contest.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, type ReactNode } from "react";
+import { createContext, useContext, useReducer, type ReactNode } from "react";
 
 type Timer = {
 	name: string;
@@ -8,6 +8,11 @@ type Timer = {
 type TimersSate = {
 	isRunning: boolean;
 	timers: Timer[];
+};
+
+const initialState: TimersSate = {
+	isRunning: true,
+	timers: [],
 };
 
 type TimersContextValue = TimersSate & {
@@ -31,20 +36,65 @@ export const useTimersContext = () => {
 type TimersContextProviderProps = {
 	children: ReactNode;
 };
+
+type StartTimersAction = {
+	type: "START_TIMERS";
+};
+type StopTimersAction = {
+	type: "STOP_TIMERS";
+};
+type AddTimerAction = {
+	type: "ADD_TIMER";
+	payload: Timer;
+};
+
+type Action = StartTimersAction | StopTimersAction | AddTimerAction;
+
+const timersReducer = (state: TimersSate, action: Action): TimersSate => {
+	if (action.type === "START_TIMERS") {
+		return {
+			...state,
+			isRunning: true,
+		};
+	}
+	if (action.type === "STOP_TIMERS") {
+		return {
+			...state,
+			isRunning: false,
+		};
+	}
+	if (action.type === "ADD_TIMER") {
+		return {
+			...state,
+			timers: [
+				...state.timers,
+				{
+					name: action.payload.name,
+					duration: action.payload.duration,
+				},
+			],
+		};
+	}
+
+	return state;
+};
+
 export const TimersContextProvider = ({
 	children,
 }: TimersContextProviderProps) => {
+	const [timersState, dispatch] = useReducer(timersReducer, initialState);
+
 	const ctx: TimersContextValue = {
-		timers: [],
-		isRunning: false,
+		timers: timersState.timers,
+		isRunning: timersState.isRunning,
 		addTimer(timerData) {
-			// ...
+			dispatch({ type: "ADD_TIMER", payload: timerData });
 		},
 		startTimers() {
-			// ...
+			dispatch({ type: "START_TIMERS" });
 		},
 		stopTimers() {
-			// ...
+			dispatch({ type: "STOP_TIMERS" });
 		},
 	};
 
