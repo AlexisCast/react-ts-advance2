4afcbb35de4428acc4040a11c294f90337ab24dc
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-31 20:58:51 -0600

    addTimers context

--- src/components/AddTimer.tsx
+++ src/components/AddTimer.tsx
@@ -4,13 +4,20 @@ import { Button } from "./UI/Button.tsx";
 import { Form as FormImp, type FormHandle } from "./UI/FormImperative.tsx";
 
 import { Input as InputR } from "./UI/InputRef.tsx";
+import { useTimersContext } from "../store/timers-contest.tsx";
 
 export default function AddTimer() {
 	const form = useRef<FormHandle>(null);
+	const { addTimer } = useTimersContext();
 
 	function handleSaveTimer(data: unknown) {
 		const extractedData = data as { name: string; duration: string };
+
 		console.log(extractedData);
+		addTimer({
+			name: extractedData.name,
+			duration: +extractedData.duration,
+		});
 		form.current?.clear();
 	}
 
--- src/components/Timers.tsx
+++ src/components/Timers.tsx
@@ -1,3 +1,16 @@
-export default function Timers() {
-	return <ul>{/* TODO ... */}</ul>;
-}
+import { useTimersContext } from "../store/timers-contest";
+import { Timer } from "./Timer.tsx";
+
+export const Timers = () => {
+	const { timers } = useTimersContext();
+
+	return (
+		<ul>
+			{timers.map((timer) => (
+				<li key={timer.name}>
+					<Timer {...timer} />
+				</li>
+			))}
+		</ul>
+	);
+};
